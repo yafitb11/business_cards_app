5e7a7efd879e5955cf729db98a493e0c39f7d666
yafitb11 <yafitb11@gmail.com>
2025-09-17 12:56:48 +0300

    public1

--- public/routes/publicController.js
+++ public/routes/publicController.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.get("/project_description", (req, res, next) => {
-    const filePath = path.join(__dirname, "../../public", "project_description.doc");
+router.get("/project_description.docx", (req, res, next) => {
+    const filePath = path.join(__dirname, "../../public", "project_description.docx");
 
-    res.download(filePath, "project_description.doc", (err) => {
+    res.download(filePath, "project_description.docx", (err) => {
         if (err) {
             next(err);
         }
