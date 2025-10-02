188c4b17d50e8ebf83e86216b23d47a4af76cb22
yafitb11 <yafitb11@gmail.com>
2025-10-02 16:01:49 +0300

    errorlogger

--- logger/loggers/morganLogger.js
+++ logger/loggers/morganLogger.js
@@ -1,10 +1,14 @@
 const chalk = require("chalk");
 const morgan = require("morgan");
+const errorLogger = require("./errorLogger");
 
 const morganLogger =
     morgan(
         (tokens, req, res) => {
             const status = Number(tokens.status(req, res));
+
+            errorLogger(req, res);
+
             const statusMessage = [
                 tokens.date(req, res, "iso"),
                 tokens.method(req, res),
@@ -14,7 +18,6 @@ const morganLogger =
                 tokens["response-time"](req, res), "ms",
             ].join(" ");
 
-            //   return status >= 400 ? chalk.red(statusMessage) : chalk.green(statusMessage);
             return (status >= 400 ? chalk.red : chalk.green)(statusMessage);
 
         }
